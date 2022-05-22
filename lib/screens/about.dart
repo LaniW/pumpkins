9933280811f1cc6e4c3d28448a2627b377a9846f
Lani <lanisuin@gmail.com>
2022-05-22 11:38:40 -0400

    About page

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -15,9 +15,23 @@ class About extends StatelessWidget {
             )),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: const Text('Open route'),
-          onPressed: () {},
+        child: ListView(
+          children: [
+            Center(
+                child: Row(
+              children: [
+                Image.asset('assets/images/IMG_4212.jpg',
+                    height: 196, width: 196),
+                Image.asset('assets/images/IMG_7891.JPG',
+                    height: 196, width: 196),
+              ],
+            )),
+            const Text('', style: TextStyle(fontSize: 20)),
+            const Center(
+                child: Text(
+                    'People are expected to spend over 3.3 billion dollars on Halloween costumes this year. 7 million of those costumes are thrown out after the holiday is over, the equivalent of 83 million plastic bottles. To help slow the environmental impact of this holiday, Leah (left) & Lani (right) created "Pumpkins", an app where people can rent, buy and give away their costumes to one another without having to go out to get a new one.',
+                    style: TextStyle(fontSize: 20))),
+          ],
         ),
       ),
     );
