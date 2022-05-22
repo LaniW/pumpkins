a7ca1d35a29c49f64dffb237cdbab6166173ece9
Lani <lanisuin@gmail.com>
2022-05-22 16:11:57 -0400

    profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -7,19 +7,35 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile',
-            style: TextStyle(
-              fontStyle: GoogleFonts.robotoMono().fontStyle,
-              fontWeight: FontWeight.bold,
-            )),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Open route'),
-          onPressed: () {},
+        appBar: AppBar(
+          title: Text('Profile',
+              style: TextStyle(
+                fontStyle: GoogleFonts.robotoMono().fontStyle,
+                fontWeight: FontWeight.bold,
+              )),
         ),
-      ),
-    );
+        body: Center(
+            child: ListView(
+          children: [
+            const Text('', style: TextStyle(fontSize: 20)),
+            Center(
+                child: Text(
+              'Hello, Leah!',
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: GoogleFonts.robotoMono().fontStyle,
+              ),
+            )),
+            Center(
+                child: Image.asset('assets/images/IMG_4212.jpg',
+                    height: 350, width: 350)),
+            const Text('', style: TextStyle(fontSize: 10)),
+            Center(
+                child: ElevatedButton(
+              child: const Text('Shopping Bag'),
+              onPressed: () {},
+            )),
+          ],
+        )));
   }
 }
