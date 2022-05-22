73ad6397d8f53d5e9cb71a566af231942c5d0a37
Lani <lanisuin@gmail.com>
2022-05-22 10:40:28 -0400

    login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pumpkins/screens/profile.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -14,12 +15,69 @@ class Login extends StatelessWidget {
               fontWeight: FontWeight.bold,
             )),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Open route'),
-          onPressed: () {},
-        ),
-      ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 60.0),
+            child: Center(
+              child: SizedBox(
+                  width: 200,
+                  height: 150,
+                  child: Image.asset('assets/images/pumpkin_icon.png')),
+            ),
+          ),
+          const Text('', style: TextStyle(fontSize: 15)),
+          const Padding(
+            padding:
+                EdgeInsets.only(left: 15.0, right: 15.0, top: 0, bottom: 0),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  hintText: 'Enter a valid email address'),
+            ),
+          ),
+          const Padding(
+            padding:
+                EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter your password'),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Forgot Password?',
+              style: TextStyle(color: Colors.orange, fontSize: 15),
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 250,
+            decoration: BoxDecoration(
+                color: Colors.orange, borderRadius: BorderRadius.circular(20)),
+            child: TextButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Profile()));
+              },
+              child: const Text(
+                'Login',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 130,
+          ),
+          const Text('Create an Account')
+        ],
+      )),
     );
   }
 }
